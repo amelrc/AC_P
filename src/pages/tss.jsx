5089db69bdce0b09bfe700e33204ba068ad7ee2e
Amel Ramirez <amelramirez@Amels-iMac.local>
2021-07-04 07:59:56 +0200

    adding styles in useffect

--- src/pages/tss.jsx
+++ src/pages/tss.jsx
@@ -1,6 +1,9 @@
+import React, { useState, useEffect, useRef } from "react";
 import styled from "styled-components";
+import { useViewportScroll, motion, useTransform } from "framer-motion";
 import device from "../generalStyles";
 import LAPTOP from "../assets/TSS/AC_P_Laptop.svg";
+import LOGO from "../assets/TSS/AC_P_TSS_Logo.svg";
 import HomePage from "../assets/TSS/TSS Landing Page Desktoptss.png";
 import ProjectPage from "../assets/TSS/TSS Project Page Desktotss.png";
 
@@ -21,34 +24,100 @@ const TextWrapper = styled.div`
     font-size: 14px;
     line-height: 18px;
   }
+  @media ${device.tablet} {
+    width: 60%;
+    margin-right: 10%;
+    padding-top: 140px;
+  }
 `;
 
-const Laptop = styled.img`
+const LogoTSS = styled(motion.img)`
+  width: 40px;
+  position: absolute;
+  top: 69%;
+  @media (orientation: landscape) {
+    top: 100%;
+  }
+  @media ${device.tablet} {
+    top: 55%;
+  }
+  @media ${device.small} {
+    top: 60%;
+  }
+`;
+
+const Laptop = styled(motion.img)`
   position: sticky;
   top: 30%;
   width: 320px;
   margin: 40px auto;
   @media ${device.tablet} {
     width: calc(84% - 54px);
+    top: 40%;
+  }
+  @media ${device.small} {
+    width: calc(75% - 54px);
+    top: 26%;
   }
 `;
 
 const Img = styled.img`
-  width: 260px;
+  width: 252px;
   margin: auto;
   @media ${device.tablet} {
-    width: calc(100% - 54px);
+    width: calc(68% - 54px);
+    margin-top: 10%;
   }
 `;
 const ImageWrapper = styled.div`
   display: flex;
   flex-direction: column;
-  ${Img}:nth-child(3) {
+  align-items: center;
+  ${Img}:nth-child(4) {
+    margin-top: 100px;
     padding-bottom: 300px;
+  }
+  @media (orientation: landscape) {
+    ${Img}:nth-child(4) {
+      margin-top: 100px;
+      padding-bottom: 98px;
+    }
   }
 `;
 
 const Tss = () => {
+  const { scrollYProgress } = useViewportScroll();
+  const ref = useRef();
+  const opacity = useTransform(scrollYProgress, [0, 0.1], [1, 0]);
+  const scale = useTransform(scrollYProgress, [0, 0.1], [1, 1.5]);
+
+  const getWindowDimensions = () => {
+    const { innerWidth: width, innerHeight: height } = window;
+    return { width, height };
+  };
+
+  const useWindowDimensions = () => {
+    const [windowDimensions, setWindowDimensions] = useState(
+      getWindowDimensions()
+    );
+
+    useEffect(() => {
+      const handleResize = () => setWindowDimensions(getWindowDimensions());
+
+      // console.log(width >= 1024 ? ref.current.style.scale : "");
+      // console.log("inside effect", width);
+
+      window.addEventListener("resize", handleResize);
+      return () => window.removeEventListener("resize", handleResize);
+    }, []);
+
+    return windowDimensions;
+  };
+
+  const { height, width } = useWindowDimensions();
+
+  console.log("hello", width);
+
   return (
     <div>
       <TextWrapper>
@@ -67,8 +136,10 @@ const Tss = () => {
           with responsive interactive prototyping for the wireframe and design.
         </p>
       </TextWrapper>
+
       <ImageWrapper>
-        <Laptop src={LAPTOP} alt="laptop frame" />
+        <LogoTSS style={{ opacity: opacity }} src={LOGO} alt="logo" />
+        <Laptop ref={ref} src={LAPTOP} alt="laptop frame" />
         <Img src={HomePage} alt="desktop" />
         <Img src={ProjectPage} alt="project" />
       </ImageWrapper>

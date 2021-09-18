6376fbcf74e6c44807eeeee3dc8c3a2eea540911
Amel Ramirez <amelrc@yahoo.com>
2021-09-18 15:48:39 +0200

    cleaned app js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import styled from "styled-components";
 import Logo from "./assets/logo.svg";
-
 import { HashRouter, Route, Switch, Link } from "react-router-dom";
 import Nav from "./components/nav";
-import TSS from "./pages/tss";
 import BSP from "./pages/bspSoftware";
 import Menu from "./pages/menu";
 import About from "./pages/about";
@@ -13,16 +11,6 @@ import Logos from "./pages/logos";
 import Rubrics from "./pages/rubrics";
 import Freelance from "./pages/freelance";
 
-const NavBar = styled.div`
-  position: fixed;
-  right: 0;
-  width: 100vw;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  align-content: space-between;
-  height: 100vh;
-`;
 const LogoWrapper = styled.div`
   display: flex;
   justify-content: flex-end;
@@ -34,16 +22,13 @@ const App = () => {
   return (
     <div id="app">
       <HashRouter>
-        {/* <NavBar> */}
         <LogoWrapper id="logo wrapper">
           <Link to="/" style={{ backgroundColor: "#0d0b33", height: 54 }}>
             <img width="58px" src={Logo} alt="logo" />
           </Link>
         </LogoWrapper>
         <Nav />
-        {/* </NavBar> */}
         <Switch>
-          {/* <Route exact path="/website/the-side-studio" component={TSS} /> */}
           <Route exact path="/software/blue-star-planning" component={BSP} />
           <Route exact path="/branding/logos" component={Logos} />
           <Route exact path="/software/rubrics" component={Rubrics} />
@@ -52,7 +37,6 @@ const App = () => {
           <Route exact path="/about" component={About} />
           <Route exact path="/contact" component={Contact} />
           <Route exact path="/" component={Home} />
-          {/* <Route exact path="/gallery" component={Gallery} /> */}
         </Switch>
       </HashRouter>
     </div>
